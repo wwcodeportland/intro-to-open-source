ba68da08470e2f7ea515cd011afb5213be5ea097
VerteDinde <keeleymhammond@gmail.com>
2017-08-22 17:12:57 -0700

    Add doc files

---
+++ js/button.js
@@ -0,0 +1,17 @@
+// Resources:
+// * [Document.createElement() - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+// * [Node.appendChild() - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild)
+// * [When to use the Button Element - CSS Tricks](https://css-tricks.com/use-button-element/)
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("clock", function() {
+  alert("did something");
+});
